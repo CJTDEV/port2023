8b349fe86afec0b53e649904935a8a6b291dba2b
CJTDEV <108761469+CJTDEV@users.noreply.github.com>
2023-08-17 16:35:42 +0200

    Revert "dont know"
    This reverts commit e091029076bab8ab6d4f7e0614fad56c1c8e58cb.

--- test/app/layout.jsx
+++ test/app/layout.jsx
@@ -1,16 +1,13 @@
 import './styles/globals.css'
-import { Inter } from 'next/font/google'
+import { Ubuntu } from '@next/font/google'
 
 //fonts
-const inter = Inter({
-  subsets: ['latin'],
-  display: 'swap',
-})
+const ubuntu = Ubuntu({ weight: '400', })
 
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={inter.className}>
+    <html lang="en" className={ubuntu.className}>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
